63a507e94340e4a0f16d336bc69d64b4bfc05c30
ViiesTech <viistech.developer@gmail.com>
2024-03-08 23:52:20 +0500

    atlas updated

--- connect_DB/Connect_DB.js
+++ connect_DB/Connect_DB.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose'); // include mongodb package
 const Connect_DB = () => {
 
     mongoose.set('strictQuery', true);
-    mongoose.connect('mongodb://localhost:27017/Fall_In_Veteran');
+    mongoose.connect('mongodb+srv://taharasheedd666:Tfllg5IkMhDgOKyH@fallinveteran.lqcnj45.mongodb.net/');
     const db = mongoose.connection;
-    db.on("error",(error)=>console.log(error));
+    db.on("error",(error)=>console.log("error",error));
     db.once("open",()=>console.log("DB Connected"));
 }
 
